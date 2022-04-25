892e9c73d6bcec5f60d25a0d6553a54956269b3c
Victor Ramirez <vramirez75@gmail.com>
2022-04-24 20:06:15 -0700

    add shell project

---
+++ GuessingGame/app/src/main/java/com/ramirez/android/guessinggame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ramirez.android.guessinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
