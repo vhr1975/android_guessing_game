75648f998b26ae2088308bed16b835abc736858f
Victor Ramirez <vramirez75@gmail.com>
2022-04-24 22:13:02 -0700

    added basic parts to the shell project

--- GuessingGame/app/src/main/java/com/ramirez/android/guessinggame/MainActivity.kt
+++ GuessingGame/app/src/main/java/com/ramirez/android/guessinggame/MainActivity.kt
@@ -2,10 +2,25 @@ package com.ramirez.android.guessinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    //on create function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //on click button function and pass in super class of View for all widgets
+    fun leftButtonClick(view:View)
+    {
+        // variable and call findViewById to access its View object and pass in the widgets unique id
+        val leftButton = findViewById<Button>(R.id.btnLeft)
+        // update the text on the button widget
+        leftButton.text = "1"
+        // TODO  increment the value after every click
+    }
+
 }
